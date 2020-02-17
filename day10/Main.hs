bade70cc73f0e744e2d65187df4cf8fe093f3d44
rsrsl <riccardosven@gmail.com>
2020-02-17 20:16:08 +0100

    Added documentation

--- day10/Main.hs
+++ day10/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import           Common     (getNumbers)
+import           Common        (getNumbers)
+import           Control.Monad
 import           Data.List
-import qualified Data.Map   as M
+import qualified Data.Map      as M
 import           Data.Maybe
 import           Text.Regex
-import Control.Monad
- -- The output type describes who should receive the token and the value of the token
 
+-- | Models a chip transaction (the act of sending a chip either to another bot or to the output)
 data Value = ToBot
     { tgt :: Int
     , val :: (Maybe Int)
@@ -18,15 +18,18 @@ data Value = ToBot
     }
     deriving (Eq, Show)
 
+-- | Models a bot together
 data Bot = Bot
-    { low   :: Value -- who should receive the low value
-    , high  :: Value -- who should receive the high value
-    , chips :: [Int] -- the chips currently in possession of this robot
+    { low   :: Value
+    , high  :: Value
+    , chips :: [Int]
     }
     deriving (Eq, Show)
 
+-- | A set of bots
 type Bots = M.Map Int Bot
 
+-- | Parse a string into a Bot
 parseBot :: String -> Maybe (Int, Bot)
 parseBot xs =
   case matchRegex
@@ -47,25 +50,30 @@ parseBot xs =
                 then ToBot idxhigh Nothing
                 else ToOutput idxhigh Nothing
 
+-- | Parse a string into a Value
 parseValue :: String -> Maybe Value
 parseValue xs =
   case take 3 xs of
-    "val" -> let w = getNumbers xs in Just $ ToBot (w !! 1) (Just (w !! 0))
+    "val" ->
+      let w = getNumbers xs
+       in Just $ ToBot (w !! 1) (Just (w !! 0))
     _ -> Nothing
 
+-- | Process a list of value messages giving chips to the corresponding bots
 processValues :: Bots -> [Value] -> Bots
 processValues bs [] = bs
 processValues bs ((ToOutput _ _):vs) = processValues bs vs
 processValues _ ((ToBot _ Nothing):_) = error "WTF?!"
 processValues bs ((ToBot i (Just x)):vs) = processValues bs' vs
   where
-      bs' = M.adjust (\o -> o {chips = x : chips o}) i bs
+    bs' = M.adjust (\o -> o {chips = x : chips o}) i bs
 
-processBot :: [Value] -> Bot ->  ([Value],  Bot)
-processBot prev b 
-    | length xs < 2 = (prev, b)
-    | length xs > 2 =error "WTF?!"
-    | otherwise = (prev ++ [lval, hval], b')
+-- | Process a single bot appending to the current value messages
+processBot :: [Value] -> Bot -> ([Value], Bot)
+processBot prev b
+  | length xs < 2 = (prev, b)
+  | length xs > 2 = error "WTF?!"
+  | otherwise = (prev ++ [lval, hval], b')
   where
     xs = chips b
     [xl, xh] = sort xs
@@ -73,9 +81,11 @@ processBot prev b
     hval = (high b) {val = Just xh}
     b' = b {chips = [], low = lval, high = hval}
 
+-- | Process all bots resulting in a new list of value messages
 processBots :: Bots -> ([Value], Bots)
 processBots bs = M.mapAccum processBot [] bs
 
+-- | Run the process until no further transactions are made
 process :: (Bots, [Value]) -> (Bots, [Value])
 process (b, []) = (b, [])
 process (b, v) = process (b'', v'')
@@ -83,35 +93,37 @@ process (b, v) = process (b'', v'')
     b' = processValues b v
     (v'', b'') = processBots b'
 
+-- | Solution to the first step
 star1 :: Bots -> IO ()
-star1 bs = flip forM_ checkSolution  $ zip [0..] (M.elems bs)
-    where
-        checkSolution :: (Int, Bot) -> IO ()
-        checkSolution (i, b) =
-            if val (low b) == Just 17 && val (high b) == Just 61
-            then do
-                putStrLn $ "Solution: " ++ show i
-            else do
-                return ()
+star1 bs = flip forM_ checkSolution $ zip [0 ..] (M.elems bs)
+  where
+    checkSolution :: (Int, Bot) -> IO ()
+    checkSolution (i, b) =
+      if val (low b) == Just 17 && val (high b) == Just 61
+        then do
+          putStrLn $ "Solution: " ++ show i
+        else do
+          return ()
 
+-- | Solution to the second step
 star2 :: Bots -> IO ()
 star2 bs = putStrLn $ "Solution: " ++ (show n)
   where
-      n = M.foldl checkSolution 1 bs
-      checkSolution :: Int -> Bot -> Int
-      checkSolution i (Bot (ToOutput 0 (Just x)) _ _)  = i * x
-      checkSolution i (Bot (ToOutput 1 (Just x)) _ _)  = i * x
-      checkSolution i (Bot (ToOutput 2 (Just x)) _ _)  = i * x
-      checkSolution i (Bot _ (ToOutput 0 (Just x)) _)  = i * x
-      checkSolution i (Bot _ (ToOutput 1 (Just x)) _)  = i * x
-      checkSolution i (Bot _ (ToOutput 2 (Just x)) _)  = i * x
-      checkSolution i (Bot _ _ _                    )  = i
+    n = M.foldl checkSolution 1 bs
+    checkSolution :: Int -> Bot -> Int
+    checkSolution i (Bot (ToOutput 0 (Just x)) _ _) = i * x
+    checkSolution i (Bot (ToOutput 1 (Just x)) _ _) = i * x
+    checkSolution i (Bot (ToOutput 2 (Just x)) _ _) = i * x
+    checkSolution i (Bot _ (ToOutput 0 (Just x)) _) = i * x
+    checkSolution i (Bot _ (ToOutput 1 (Just x)) _) = i * x
+    checkSolution i (Bot _ (ToOutput 2 (Just x)) _) = i * x
+    checkSolution i (Bot _ _ _)                     = i
 
 main :: IO ()
 main = do
   s <- readFile "input"
   let robots = M.fromList . catMaybes . map parseBot . lines $ s
-  let values =  catMaybes . map parseValue . lines $ s
+  let values = catMaybes . map parseValue . lines $ s
   let (robots', _) = process (robots, values)
   star1 robots'
   star2 robots'
